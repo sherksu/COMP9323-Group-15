0a56acde947e1dbdfb77e878c6716567e5b7c571
dadiaomengmei07 <56289104+dadiaomengmei07@users.noreply.github.com>
2019-11-03 18:11:39 +1100

    Update views.py

--- app/profile/views.py
+++ app/profile/views.py
@@ -55,10 +55,11 @@ def ranking():
 @profile.route('/question_set/')
 def question_set():
     dic = get_user_info(current_user.username)
+    print(dic)
     return render_template("/profile/question_set.html", **dic, get_question_content=get_question_content,
                            get_chapter_name=get_chapter_name, get_course_name=get_course_name,
                            get_course_code=get_course_code, get_question_course=get_question_course,
-                           get_question_chapter=get_question_chapter)
+                           get_question_chapter=get_question_chapter,correct_answer = correct_answer)
 
 
 @profile.route('/study_career/')
@@ -105,3 +106,13 @@ def get_question_chapter(question_id):
     question = db.question_set.find_one({"_id": question_id})
     if question:
         return question['chapter']
+    
+def correct_answer(str):
+    if str == '1':
+        return 'A'
+    elif str == '2':
+        return 'B'
+    elif str == '3':
+        return 'C'
+    elif str == '4':
+        return 'D'
