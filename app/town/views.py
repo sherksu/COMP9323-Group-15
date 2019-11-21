e800a1ed73b5a4504c09f8b167f2094d38b3156d
Ian Jiang <Iandustbin@gmail.com>
2019-11-22 06:38:22 +1100

    Update views.py

--- app/town/views.py
+++ app/town/views.py
@@ -17,7 +17,8 @@ def entry_town(course):
     chapters = db.chapters.find({'course': course_id})
     get_node = db.knowledge_nodes.find
 
-    level = get_list_of_levels(user_id, course_id)['levels'][0]
+    # level = get_list_of_levels(user_id, course_id)['levels'][0]
+    level = 1
     return render_template('/town/my_town.html',  username=current_user.username, level=level, course=course,
                            is_town=True, code=code, _id=course_id, name=name, chapters=list(chapters),
                            get_node=get_node)
