3de89758fe4323adfb95886b40b458b6d6dfb914
sherksu <sharksu41807@hotmail.com>
2019-11-25 04:38:25 +1100

    update new user no level bug

--- app/town/views.py
+++ app/town/views.py
@@ -15,7 +15,10 @@ def entry_town(course):
     course_id = course['_id']
     chapters = db.chapters.find({'course': course_id})
     get_node = db.knowledge_nodes.find
-    level = get_list_of_levels(user_id, course_id)['levels'][0]
+    if get_list_of_levels(user_id, course_id)['levels']:
+        level = get_list_of_levels(user_id, course_id)['levels'][0]
+    else:
+        level = None
     return render_template('/town/my_town.html',  username=current_user.username, level=level, course=course,
                            is_town=True, code=code, _id=course_id, name=name, chapters=list(chapters),
                            get_node=get_node)
