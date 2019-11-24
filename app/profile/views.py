7357bb9317b9c385d2d8d583f166072116cd71f0
dadiaomengmei07 <56289104+dadiaomengmei07@users.noreply.github.com>
2019-11-24 20:34:03 +1100

    123
    123

--- app/profile/views.py
+++ app/profile/views.py
@@ -66,25 +66,6 @@ def question_set():
                            get_question_chapter=get_question_chapter,correct_answer = correct_answer,
                            get_question_option=get_question_option,get_question_correct_answer=get_question_correct_answer,
                            chr=chr,int=int,str=str)
-<<<<<<< HEAD
-
-@profile.route('/question_set_solutions/<question_id>')
-def question_set_solutions(question_id):
-    question = get_question(question_id)
-    q_id = question['_id']
-    content = question['content']
-    option = question['option']
-    correct_answer = question['correct_answer']
-    solution = question['solutions']
-    dic = get_user_info(current_user.username)
-    for i in dic['error_set']:
-        if i['question'] == q_id:
-            user_answer = int(i['answer'])
-    print(user_answer)
-    return render_template("/profile/question_set_solutions.html",content=content, option=option, correct_answer=correct_answer,
-                           solutions=solution, chr=chr, id=q_id,user_answer = user_answer)
-=======
->>>>>>> parent of bffd2e2... 123
 
 @profile.route('/study_career/')
 def study_career():
