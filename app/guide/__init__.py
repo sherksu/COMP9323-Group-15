ead0facd54681cc54f6162882bc1291b2c2a601d
sherksu <sharksu41807@hotmail.com>
2019-11-25 03:39:51 +1100

    update guide (add signup button at login page)

--- app/guide/__init__.py
+++ app/guide/__init__.py
@@ -1,3 +1,10 @@
+"""
+# Project           : COMP9323
+# Author            : Heping Zhao
+# Date created      : 25/10/2019
+# Description       : GUIDE SYSTEM BLUEPRINT
+"""
+
 from flask import Blueprint
 
 guide = Blueprint('guide', __name__)
--- app/guide/email.py
+++ app/guide/email.py
@@ -1,3 +1,9 @@
+"""
+# Project           : COMP9323
+# Author            : Heping Zhao
+# Date created      : 25/10/2019
+# Description       : GUIDE SYSTEM -- Send reset password email
+"""
 from flask_mail import Message
 from app import mail
 from SevManager import app
@@ -14,5 +20,6 @@ def send_reset_password_mail(user, token):
     msg = Message('[Data Hunter Team]Reset your password',
                   sender=current_app.config['MAIL_USERNAME'],
                   recipients=[user.find_email()],
-                  html=render_template('/guide/reset_password_mail.html', user=user, token=token))
+                  html=render_template('/guide/send_mail.html', user=user, token=token))
     Thread(target=send_asunc_mail, args=(app, msg, )).start()
+
