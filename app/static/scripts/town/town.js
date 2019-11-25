89fcd88848e951e99ad907fcaeeb39c02b6960f4
sherksu <sharksu41807@hotmail.com>
2019-11-25 13:37:19 +1100

    update town

--- app/static/scripts/town/town.js
+++ app/static/scripts/town/town.js
@@ -2,9 +2,33 @@ $('document').ready(function() {
    document.onkeydown = function() {
        if (event.keyCode === 13) {
            let chat = $('.chat-input');
-           console.log(chat.val()); // emit to socket
-           chat.val("");  // clear textfield
-           chat.fadeToggle();
+           $('#chats').fadeToggle();
+           if ( chat.val() ) {
+               console.log(chat.val()); // emit to socket
+               chat.val(""); // clear up textfield
+           } else {
+               chat.focus();
+           }
        }
-   }
+   };
+
+   $('.single_btn').on('click', () => {
+       $('#single_content').fadeIn();
+       $('#multiple_content').css('display', 'none');
+   });
+
+   $('.multiple_btn').on('click', () => {
+       $('#multiple_content').fadeIn();
+       $('#single_content').css('display', 'none');
+   });
+
+   $('#beginner').on('click', () => {
+        $('#beginner_content').fadeIn();
+        $('#expert_content').css('display', 'none');
+    });
+
+    $('#expert').on('click', () => {
+        $('#beginner_content').css('display', 'none');
+        $('#expert_content').fadeIn();
+    });
 });
